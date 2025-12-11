36c6c3c70c99ad0c8a669809c5fec97effb2f690
Meteoronyx <xseriesprogrammer@gmail.com>
2025-12-11 11:46:25 +0700

    add PM2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [{
+    name: 'dbspot',
+    script: 'src/server.js',
+    instances: 'max',
+    exec_mode: 'cluster',
+    env: {
+      NODE_ENV: 'development',
+      PORT: 3000
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 80
+    },
+    log_file: './logs/pm2-combined.log',
+    error_file: './logs/pm2-error.log',
+    out_file: './logs/pm2-out.log',
+    log_date_format: 'YYYY-MM-DD HH:mm Z',
+    max_memory_restart: '1G',
+    kill_timeout: 5000,
+    listen_timeout: 3000
+  }]
+};
